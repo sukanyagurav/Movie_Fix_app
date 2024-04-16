284fc3f354abfb24f0457f8a4c6bb50722a9423e
unknown <sukanyagurav6@gmail.com>
2024-04-16 21:27:42 +0530

    created projects

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+const App = () => {
+  return (
+    <>
+    <header>
+      <h1>MovieFix</h1>
+    </header>
+   
+
+    </>
+  )
+}
+
+export default App
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import './globals.css'
+import App from './components/App'
+const container = document.getElementById('root')
+const root = createRoot(container)
+root.render(<App/>)
